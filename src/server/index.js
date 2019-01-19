09d168a3fb873d5b8dfbb85a2dbd6d821cebd889
MCVNick <mcvnick@gmail.com>
2019-01-19 15:46:30 -0700

    commented all my stuff on index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,53 @@
+// this will be used to handle requests
 const express = require('express')
+// this will be used to handle anything we send in through the body
 const bodyParser = require('body-parser')
 
+//here we are importing the controller which will handle all the requests
 const magicController = require('./controller')
 
+//here we are saying that this is equal to express (a server helper)
 const app = express()
+//here we are saying that it should use bodyParser to format whatever we send in as a json
 app.use(bodyParser.json())
 
+
+
+//here are all the requests that we will handle
+
+//Get all the magic cards from catalog (limited to 175 per call)
 app.get(`/api/allCards`, magicController.getAllTheMagicCards)
+
+//Get all your magic cards (not really limited, only by hardware)
 app.get(`/api/yourCards`, magicController.getYourMagicCards)
+
+//Filter your cards (unused now, but may implement later)
+//here we are passing in a param with the property filter
 app.get(`/api/yourCards/:filter`, magicController.filterYourLibrary)
+
+//Filter through the catalog
+//here we are passing in a param with the property filter
 app.get(`/api/allCards/:filter`, magicController.filterCatalogLibrary)
+
+//Get random cards from all the magic cards (all being all 200,000+ cards)
 app.get(`/api/random`, magicController.getRandomCards)
+
+//Posting cards into your library
 app.post(`/api/allCards/`, magicController.postCardToLibrary)
+
+//Updating cards in your library
+//here we are passing in a param with the property id
 app.put(`/api/allCards/:id`, magicController.updateCardQuantity)
+
+//Deleting cards in your library
+//here we are passing in a param with the property id
 app.delete(`/api/allCards/:id`, magicController.deleteCardFromLibrary)
 
+//this is the port we will be using
 const port = 3001;
+
+//then we tell it to listen on this port
 app.listen(port, () => {
+    //console will output that we are listening on that specified port
     console.log('Listening on port: ' + port)
 })
